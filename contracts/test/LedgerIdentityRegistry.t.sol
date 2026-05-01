517f96a7093abd95b87abaf1c0afee7a73c87737
Joel Offbeat <joeloffbeat@gmail.com>
2026-05-01 21:30:00 +0530

    contracts/test: LedgerIdentityRegistry initial test cases

---
+++ contracts/test/LedgerIdentityRegistry.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {LedgerIdentityRegistry} from "../src/LedgerIdentityRegistry.sol";
+
+contract LedgerIdentityRegistryTest {
+    function testRegisterAndLookup() external {
+        LedgerIdentityRegistry registry = new LedgerIdentityRegistry();
+        address worker = address(0xA11CE);
+        registry.registerAgent(worker, "worker-001.ledger.eth", "who,pay,tx,rep,mem");
+
+        LedgerIdentityRegistry.AgentIdentity memory identity = registry.getAgent(worker);
+        require(identity.agentAddress == worker, "agent");
+        require(keccak256(bytes(identity.ensName)) == keccak256("worker-001.ledger.eth"), "ens");
+        require(keccak256(bytes(identity.capabilities)) == keccak256("who,pay,tx,rep,mem"), "capabilities");
+    }
+}
+
+    // TODO: more cases
+}
