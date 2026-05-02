f06bf63524ba652d78261b34c9a2fedfabead204
Joel Offbeat <joeloffbeat@gmail.com>
2026-05-02 18:18:00 +0530

    contracts/test: LedgerIdentityRegistry full test suite

--- contracts/test/LedgerIdentityRegistry.t.sol
+++ contracts/test/LedgerIdentityRegistry.t.sol
@@ -15,6 +15,3 @@ contract LedgerIdentityRegistryTest {
         require(keccak256(bytes(identity.capabilities)) == keccak256("who,pay,tx,rep,mem"), "capabilities");
     }
 }
-
-    // TODO: more cases
-}
