d2c6e5b3af74f1135f6db952d13cc7d7eb8a2bfb
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2026-05-02 17:30:00 +0530

    contracts/script: Deploy.s.sol Foundry deployment for Galileo

---
+++ contracts/script/Deploy.s.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {LedgerEscrow} from "../src/LedgerEscrow.sol";
+import {LedgerIdentityRegistry} from "../src/LedgerIdentityRegistry.sol";
+import {MockTEEOracle} from "../src/MockTEEOracle.sol";
+import {WorkerINFT} from "../src/WorkerINFT.sol";
+
+interface Vm {
+    function envUint(string calldata name) external view returns (uint256);
+    function envOr(string calldata name, address defaultValue) external view returns (address);
+    function startBroadcast(uint256 privateKey) external;
+    function stopBroadcast() external;
+}
+
+contract Deploy {
+    Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+    bytes private constant DEMO_VALID_PROOF = "ledger-valid-tee-proof";
+
+    function run()
+        external
+        returns (
+            MockTEEOracle oracle,
+            WorkerINFT workerINFT,
+            LedgerEscrow escrow,
+            LedgerIdentityRegistry identityRegistry
+        )
+    {
+        uint256 privateKey = vm.envUint("PRIVATE_KEY");
+        address reputationRegistry =
+            vm.envOr("ERC8004_REGISTRY_ADDR", address(0x8004B663056A597Dffe9eCcC1965A193B7388713));
+
+        vm.startBroadcast(privateKey);
+        oracle = new MockTEEOracle(keccak256(DEMO_VALID_PROOF));
+        workerINFT = new WorkerINFT(address(oracle));
+        escrow = new LedgerEscrow(reputationRegistry);
+        identityRegistry = new LedgerIdentityRegistry();
+        vm.stopBroadcast();
+    }
+}
